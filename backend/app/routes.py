4191aa03690e105597b5db0adf4f2c1d5c4197fb
becoollll <sophia910429@gmail.com>
2025-01-27 00:20:34 -0500

    adjust recipe layout

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -3,25 +3,22 @@ from flask_cors import cross_origin
 from .gpt_recipes import generate_recipe
 import re
 
-# 使用 Blueprint 注册路由
 routes = Blueprint('routes', __name__)
 
 @routes.route('/generate_recipe', methods=['POST', 'OPTIONS'])
 @cross_origin(origins="http://localhost:3000")
 def generate_recipe_route():
     if request.method == 'OPTIONS':
-        # 处理 CORS 预检请求
         response = jsonify({"message": "CORS preflight successful"})
         response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
         response.headers.add("Access-Control-Allow-Headers", "Content-Type")
         response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
         return response, 200
 
-    # 处理 POST 请求
     data = request.json
     original_ingredients = data.get('ingredients', '')
     #print(original_ingredients)
-    ingredients = re.split(r'[,\s]+', original_ingredients.strip())
+    ingredients = re.split(r'[,\.\s]+', original_ingredients.strip())
 
 # 過濾空項（避免多餘的分隔符導致的空字串）
     ingredients= [item for item in ingredients if item]
